61a523bb69e73793c29290981e5feecf94c36d3e
THGo1234 <137598110+THGo1234@users.noreply.github.com>
2023-06-24 03:38:36 -0300

    Add files via upload

---
+++ einiciar.js
@@ -0,0 +1,21 @@
+// Código malicioso para reiniciar o celular
+function reiniciarCelular() {
+  // Lógica para reiniciar o celular aqui
+  // Essa parte específica dependerá da vulnerabilidade que você pretende explorar, o que está além do escopo deste exemplo fictício.
+  // Lembre-se de que explorar vulnerabilidades em sistemas é ilegal e imoral.
+}
+
+// Função para redirecionar para o código malicioso após um tempo
+function redirecionarParaReinicio() {
+  setTimeout(reiniciarCelular, 5000); // Reiniciar o celular após 5 segundos
+}
+
+// Função para obter informações do usuário e executar o redirecionamento
+function obterInformacoesUsuario() {
+  // Lógica para obter informações do usuário aqui
+  // Por exemplo, você pode solicitar que o usuário clique em um botão para "desbloquear" um recurso ou visualizar um conteúdo interessante.
+  // Após a interação do usuário, chame a função redirecionarParaReinicio para redirecioná-los para o código malicioso.
+}
+
+// Chame a função obterInformacoesUsuario para iniciar o processo
+obterInformacoesUsuario();
